57d308a3683c66864553dd5bad41cee0fb4cd901
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2024-12-02 11:24:51 -0300

    Got the playRound function and playGame function working

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice(){
     let random = (Math.floor(Math.random() * 3));
 
     if(random === 0){
-        return "Rock";
+        return "ROCK";
     } else if(random === 1){
-        return "Paper";
+        return "PAPER";
     } else if(random === 2){
-        return "Scissors";
+        return "SCISSORS";
     }
 }
 console.log(getComputerChoice());
@@ -18,3 +18,34 @@ function getHumanChoice(){
 }
 console.log(getHumanChoice());
 
+
+
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice){
+        if(humanChoice === computerChoice){
+            return "It is a tie";
+        } 
+        else if(humanChoice === "PAPER" && computerChoice === "ROCK" || humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+            humanScore++;
+            return `You win! You chose: ${humanChoice} and Computer chose: ${computerChoice}. ${humanChoice} beats ${computerChoice}.`;
+        }
+        else{
+            computerScore++;
+            return `You lose! You chose: ${humanChoice} and Computer chose: ${computerChoice}. ${computerChoice} beats ${humanChoice}.`
+        }
+    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(humanSelection, computerSelection));
+}
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
