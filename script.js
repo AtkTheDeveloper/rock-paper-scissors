0fdb3bfce0cc9abce8e6c37fbf1aaa22118b3a95
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2025-01-01 13:42:01 -0300

    Add buttons for user choices and update game logic to display results

--- script.js
+++ script.js
@@ -1,71 +1,75 @@
-function getComputerChoice(){
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+function getComputerChoice() {
     let random = (Math.floor(Math.random() * 3));
 
-    if(random === 0){
+    if (random === 0) {
         return "ROCK";
-    } else if(random === 1){
+    } else if (random === 1) {
         return "PAPER";
-    } else if(random === 2){
+    } else if (random === 2) {
         return "SCISSORS";
     }
 }
 
-function getHumanChoice(){
-    let userChoice = prompt(`Enter either rock, paper or scissors`);
-
-   return userChoice.toUpperCase();
+function getHumanChoice(choice) {
+    const buttons = document.querySelectorAll('button');
+    for (const button of buttons) {
+        button.addEventListener('click', (e) => {
+            let userChoice = e.target.textContent;
+            choice(userChoice.toUpperCase());
+        });
+    }
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame(){
+function playGame() {
 
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice === computerChoice){
-            return alert("It is a tie");
-        } 
-        else if(humanChoice === "PAPER" && computerChoice === "ROCK" || humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "PAPER"){
-            humanScore++;
-            return alert(`You win! You chose: ${humanChoice} and Computer chose: ${computerChoice}. ${humanChoice} beats ${computerChoice}.`);
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            div.textContent = "It is a tie";
         }
-        else{
+        else if (humanChoice === "PAPER" && computerChoice === "ROCK" || humanChoice === "ROCK" && computerChoice === "SCISSORS" || humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+            humanScore++;
+            div.textContent =`You win!`;
+        }
+        else {
             computerScore++;
-            return alert(`You lose! You chose: ${humanChoice} and Computer chose: ${computerChoice}. ${computerChoice} beats ${humanChoice}.`);
+            div.textContent =`You lose!`;
         }
     }
     //Round 1
-    const humanSelection1 = getHumanChoice();
-    const computerSelection1 = getComputerChoice();
-    console.log(playRound(humanSelection1, computerSelection1));
+    getHumanChoice((humanSelection1) => {
+        const computerSelection1 = getComputerChoice();
+        playRound(humanSelection1, computerSelection1);
+    });
 
-    //Round 2
-     const humanSelection2 = getHumanChoice();
-     const computerSelection2 = getComputerChoice();
-     console.log(playRound(humanSelection2, computerSelection2));
+//Round 2
+getHumanChoice((humanSelection2) => {
+    const computerSelection2 = getComputerChoice();
+    playRound(humanSelection2, computerSelection2);
+});
 
-    //Round 3
-    const humanSelection3 = getHumanChoice();
+//Round 3
+getHumanChoice((humanSelection3) => {
     const computerSelection3 = getComputerChoice();
-    console.log(playRound(humanSelection3, computerSelection3));
+    playRound(humanSelection3, computerSelection3);
+});
 
-    //Round 4
-     const humanSelection4 = getHumanChoice();
-     const computerSelection4 = getComputerChoice();
-     console.log(playRound(humanSelection4, computerSelection4));
+//Round 4
+getHumanChoice((humanSelection4) => {
+    const computerSelection4 = getComputerChoice();
+    playRound(humanSelection4, computerSelection4);
+});
 
-    //Round 5
-    const humanSelection5 = getHumanChoice();
+//Round 5
+getHumanChoice((humanSelection5) => {
     const computerSelection5 = getComputerChoice();
-    console.log(playRound(humanSelection5, computerSelection5));
+    playRound(humanSelection5, computerSelection5);
+});
 
-    alert(`Your total score is: ${humanScore} and computer's score is: ${computerScore}.`)
-
-    if(humanScore > computerScore){
-        alert("Hurray! You won the game");
-    } else{
-        alert("Sorry you lost! Computer Won");
-    }
 }
-
 playGame();
